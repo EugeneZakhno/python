3062061e2a717f1d5b0838cc5b6da220695ef6bf
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2024-09-18 16:16:04 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from array import array
+
+name = "Eugen"
+full_name = "Eugene Zakhno"
+PI = 3.14
+
+brand = "ZHN"
+age = 38
+numbers = []
+
+print(type(brand))
+print(type(PI))
+print(type(numbers))
