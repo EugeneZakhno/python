38dec4f21ea3c3da513ee7fb26c73d55270a98d7
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2024-09-18 17:47:21 +0300

    eighth commit

---
+++ if_statements.py
@@ -0,0 +1,10 @@
+from assigment_operator import number
+
+number = 100
+
+if number > 0:
+    print(f"number is {number} positive")
+elif number == 0:
+    print(number)
+else:
+    print(f"{number} is negative")
