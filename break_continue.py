7c47677949440597edae36319ec762964b730214
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2024-09-19 15:06:29 +0300

    eighteen commit

---
+++ break_continue.py
@@ -0,0 +1,16 @@
+number = 0
+
+while number < 10:
+    if number == 5:
+        break
+
+
+
+while number < 10:
+    if number == 5:
+        number +=1
+        continue
+else:
+    print("fuck")
+
+
