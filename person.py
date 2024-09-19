c1306218cd5cdd26f7bf30939af24be6a47c90ee
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2024-09-19 14:29:45 +0300

    thirteen commit

---
+++ person.py
@@ -0,0 +1,19 @@
+# hesh-table  or map
+# key - value , key must be unique
+
+person = {
+    "name":"Eugene",
+    "age": 30,
+    "adress": "USA"
+}
+
+print(person)
+
+print(person['age'])
+print(person.keys())
+print(person.values())
+#print(person.clear())
+#person.clear()
+print(person)
+person.get()
+print(person.get)
--- union_sets.py
+++ union_sets.py
@@ -4,10 +4,6 @@ letterSet2 = {'df','fgg'}
 
 print(letterSet1.union(letterSet2))
 print(letterSet1|letterSet2)
-
-
 print(letterSet1 & letterSet2)
-
 print(letterSet1.difference(letterSet2))
-
 print(letterSet1.symmetric_difference(letterSet2))
