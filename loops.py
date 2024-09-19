eb31ab3d41377262623c9b8fb0429eed9fe6e0de
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2024-09-19 14:47:48 +0300

    sixteen commit

--- loops.py
+++ loops.py
@@ -1,3 +1,6 @@
+from lists import numbers
+from operators import result
+
 names = {"Euegne", "Certys", 'James', "Kitty"}
 
 print(names)
@@ -24,3 +27,9 @@ for key, value in person.items():
     print(f"key {key} , value {value}")
 #############################33
 
+result = 0
+numbers = [8,9,12,77,974,48,67,784]
+
+for number in numbers:
+    result += number
+    print(f" result is {result}")
