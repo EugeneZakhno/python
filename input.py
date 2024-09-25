dd1cb33478b92951ef7c336da8b09333896ba4c2
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2024-09-25 15:08:32 +0300

    hide pwd

--- input.py
+++ input.py
@@ -1,3 +1,6 @@
+from getpass import getpass
+'''
+###
 a = input()
 print(a)
 
@@ -15,3 +18,8 @@ print("Great!" )
 len("engage")
 print(len("engage"))
 ###
+'''
+
+#Запускать не в консоли, а в терминале по команде: python input.py (название файла)
+pwd = getpass('password')
+print('You wrote your password')
